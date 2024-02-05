92e36d23c0f1f74551b3dc5d34570b5bc70fb25e
Sergio Jurado <juradotrillosergio1207@gmail.com>
2024-02-05 16:31:20 +0100

    Empezado el 03

---
+++ Proyectos/03-otros-hooks/src/components/context/TodoProvider.jsx
@@ -0,0 +1,26 @@
+import TodoContext from "./TodoContext";
+
+const initialState = [
+  {
+    id: 1,
+    nombre: "Sacar al perro",
+    completada: false,
+  },
+  {
+    id: 2,
+    nombre: "Aprender React",
+    completada: false,
+  },
+];
+
+const TodoProvider = ({ children }) => {
+  const [tareas, setTareas] = useState(initialState);
+  function eliminarTarea() {}
+  return (
+    <TodoContext.Provider value={{ tareas, setTareas, eliminarTarea }}>
+      <>{children}</>
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;
